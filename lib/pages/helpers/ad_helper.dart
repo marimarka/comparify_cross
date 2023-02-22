44e30db6777f24c28b2e3e2ca6730744c98386a4
marinapaberze <marimarka@inbox.lv>
2023-02-22 10:50:32 +0200

    real ads ids

--- lib/pages/helpers/ad_helper.dart
+++ lib/pages/helpers/ad_helper.dart
@@ -3,47 +3,47 @@ import 'dart:io';
 class AdHelper {
 
   // real
-  // static String get bannerAdUnitId {
-  //   if (Platform.isAndroid) {
-  //     return 'ca-app-pub-4344029966896694/3372828328';
-  //   } else if (Platform.isIOS) {
-  //     return 'ca-app-pub-4344029966896694/7034252021';
-  //   } else {
-  //     throw UnsupportedError('Unsupported platform');
-  //   }
-  // }
-
-  //test
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-4344029966896694/3372828328';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-4344029966896694/7034252021';
     } else {
-      throw new UnsupportedError('Unsupported platform');
-    }
-  }
-//test
-  static String get interstitialAdUnitId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/1033173712";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/4411468910";
-    } else {
-      throw new UnsupportedError("Unsupported platform");
+      throw UnsupportedError('Unsupported platform');
     }
   }
 
-  // real
-  // static String get interstitialAdUnitId {
+  //test
+  // static String get bannerAdUnitId {
   //   if (Platform.isAndroid) {
-  //     return 'ca-app-pub-4344029966896694/1449937215';
+  //     return 'ca-app-pub-3940256099942544/6300978111';
   //   } else if (Platform.isIOS) {
-  //     return 'ca-app-pub-4344029966896694/7136792407';
+  //     return 'ca-app-pub-3940256099942544/2934735716';
   //   } else {
-  //     throw UnsupportedError('Unsupported platform');
+  //     throw new UnsupportedError('Unsupported platform');
   //   }
   // }
+//test
+//   static String get interstitialAdUnitId {
+//     if (Platform.isAndroid) {
+//       return "ca-app-pub-3940256099942544/1033173712";
+//     } else if (Platform.isIOS) {
+//       return "ca-app-pub-3940256099942544/4411468910";
+//     } else {
+//       throw new UnsupportedError("Unsupported platform");
+//     }
+//   }
+
+  // real
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-4344029966896694/1449937215';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-4344029966896694/7136792407';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
 
   // static String get rewardedAdUnitId {
   //   if (Platform.isAndroid) {
